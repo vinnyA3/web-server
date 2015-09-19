e20a88734f18afabfe08c1970fe0f37caacdfe07
vinnyA3 <vinaceto@yahoo.com>
2015-09-19 18:48:23 -0400

    Initialize Repo

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var port = 3000;
+var app = express();
+
+//middleware
+var middleware = {
+    requireAuthenication: function(req,res,next){
+        console.log("private route hit!");
+        next();
+    },
+    logger: function(req,res,next){
+        console.log("Request: " + req.method + ' for: ' + req.originalUrl + " on: "+ new Date().toString() );
+        next();
+    }
+}
+//order is important here, middlewear is best specified up top
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthenication, function(req,res){
+    res.send("About Us");
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(port, function(){
+    console.log("Express server started on port: " + port);
+});
